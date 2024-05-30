53904fc84ce3f2990bce8aa305b2c8ab212b69d4
MyferIsADev <contactme.myfer@protonmail.com>
2024-05-30 18:05:26 -0500

    🔗Fix `docsLink` and `githubLink` urls for the new domain.

--- docs/app.js
+++ docs/app.js
@@ -1,7 +1,7 @@
 import render from './lib/ice-cream.js';
 
-const docsLink = "https://docs.icecream-js.pages.dev"
-const githubLink = "https://gh.icecream-js.pages.dev"
+const docsLink = "https://icecream.js.org/docs/"
+const githubLink = "https://icecream.js.org/github/"
 
 function btn(text, link) {
     return (`
